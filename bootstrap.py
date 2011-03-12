46a0def6929236f97297cbec843be0239aee4864
Zach Williams <hey@zachwill.com>
2011-03-12 02:40:33 -0600

    Removed unused modules

--- bootstrap.py
+++ bootstrap.py
@@ -1,12 +1,5 @@
-"""
-Google App Engine uses this file to run your Flask application.
+"""Google App Engine uses this file to run your Flask application."""
 
-The root directory is modified to include the `libs` folder,
-where flask, jinja2, werkzeug, and other libraries are stored.
-"""
-
-import sys
-import os
 from wsgiref.handlers import CGIHandler
 from app import app
 
