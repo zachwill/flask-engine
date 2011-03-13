899a9b2ac5541d17d66f410ec0f4bc430a5fded3
Zach Williams <hey@zachwill.com>
2011-03-13 05:00:16 -0500

    Added wtforms example code. Updated template.

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ Jinja2 Documentation:      http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:    http://werkzeug.pocoo.org/documentation/
 GAE Python Documentation:  http://code.google.com/appengine/docs/python/
 
-The `apps.py` file is used for both the routing and logic of your
-application by default, but this can be split apart similar to how
-Django splits models, views, urls, and templates.
+This file is used for both the routing and logic of your
+application by default, but it can be split apart similar to how
+Django splits views and urls into separate files.
 
-If you're wanting to use reCaptcha support for your application,
+NOTE: If you're wanting to use reCaptcha support for your application,
 you will have to obtain keys from http://recaptcha.net/ -- the keys
 provided here will only work while you're developing on localhost.
 """
@@ -20,11 +20,15 @@ adjust_sys_path()
 # You can now import from libs like normal.
 
 from flask import Flask, url_for, render_template, request, redirect
-
-RECAPTCHA_PUBLIC_KEY = '6LeYIbsSAAAAACRPIllxA7wvXjIE411PfdB2gt2J'
-RECAPTCHA_PRIVATE_KEY = '6LeYIbsSAAAAAJezaIq3Ft_hSTo0YtyeFG-JgRtu'
+from wtforms import Form, TextField, validators
 
 app = Flask(__name__)
+app.debug = True
+
+
+class TodoForm(Form):
+    """Simple todo form."""
+    todo = TextField([validators.Required()])
 
 
 @app.route('/')
@@ -36,14 +40,18 @@ def index():
 @app.route('/todo/')
 def todo_list():
     """Simple todo page."""
-    return render_template('todo.html', todos=Todo.all().order('-created_at'))
+    form = TodoForm()
+    return render_template('todo.html', form=form,
+            todos=Todo.all().order('-created_at'))
 
 
 @app.route('/todo/add', methods=["POST"])
 def add_todo():
     """Add a todo."""
-    todo = Todo(text=request.form['text'])
-    todo.save()
+    form = TodoForm(request.form)
+    if request.method == 'POST' and form.validate():
+        todo = Todo(text=form.todo.data)
+        todo.save()
     return redirect(url_for('todo_list'))
 
 
