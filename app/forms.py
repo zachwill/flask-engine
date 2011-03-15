f3355df2e200fee261aef472783556eb95299237
Zach Williams <hey@zachwill.com>
2011-03-14 19:45:00 -0500

    Now using flaskext.wtf for forms

--- app/forms.py
+++ app/forms.py
@@ -5,9 +5,9 @@ Flask WTForms Patterns:  http://flask.pocoo.org/docs/patterns/wtforms/
 Forms for your application can be stored in this file.
 """
 
-from wtforms import Form, TextField, validators
+from flaskext.wtf import Form, TextField, Required
 
 
 class TodoForm(Form):
     """Simple todo form."""
-    todo = TextField([validators.Required()])
+    todo = TextField(validators=[Required()])
