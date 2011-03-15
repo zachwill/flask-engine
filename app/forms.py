645fa437dbb6996fa9816632c109585dca0fbd53
Zach Williams <hey@zachwill.com>
2011-03-14 22:31:15 -0500

    Updated docstring

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 """
-WTForms Documentation:   http://wtforms.simplecodes.com/
-Flask WTForms Patterns:  http://flask.pocoo.org/docs/patterns/wtforms/
+WTForms Documentation:    http://wtforms.simplecodes.com/
+Flask WTForms Patterns:   http://flask.pocoo.org/docs/patterns/wtforms/
+Flask-WTF Documentation:  http://packages.python.org/Flask-WTF/
 
 Forms for your application can be stored in this file.
 """
