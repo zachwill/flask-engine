0d17dc9595eaec20e9c01d0dd66e0d93c71b1287
Zach Williams <hey@zachwill.com>
2011-03-16 11:02:20 -0500

    Added additional tests

--- test/app_tests.py
+++ test/app_tests.py
@@ -3,14 +3,16 @@ Using gaeunit.py to run tests -- while tests can't be run from the
 commandline, this is currently the best way I've found to use the Python
 unittest module.
 """
+
 import unittest
-from utils import adjust_sys_path
+from utils import find_gae_sdk, adjust_sys_path
 
 adjust_sys_path()
 
-# You can write your unittests just like normal below.
+# And just write your unittests just like normal below.
 
-from app import create_app
+import flask
+from mock import patch
 
 
 class LibsImportTest(unittest.TestCase):
@@ -19,24 +21,42 @@ class LibsImportTest(unittest.TestCase):
         import libs.flask
         self.path = libs.flask.__path__
 
-    def test_regular_import_without_lib(self):
-        import flask
-        path = flask.__path__
-        self.assertEquals(path, self.path)
-
-    def test_from_libs_import(self):
-        from libs import flask
-        path = flask.__path__
-        self.assertEquals(path, self.path)
-
-    def test_import_flaskext(self):
-        import flaskext
+    def test_regular_path_without_libs(self):
+        flask_path = flask.__path__
+        assert flask_path == self.path
 
 
 class AppTest(unittest.TestCase):
 
     def setUp(self):
-        self.app = create_app()
+        from app import create_app
+        app = create_app()
+        self.app = app.test_client()
+
+    def test_index_page(self):
+        rv = self.app.get('/')
+        assert "I'm in yo app-engine" in rv.data
+
+    def test_flask_default_redirecting(self):
+        rv = self.app.get('/todo')
+        assert 'Redirecting' in rv.data
+        assert rv.status_code == 301
+
+    def test_todo_page(self):
+        rv = self.app.get('/todo/')
+        assert 'some todos' in rv.data
+
+    def test_add_todo_csrf_is_missing(self):
+        rv = self.app.post('/todo/add', flask.json.dumps({'todo':'tests'}))
+        assert 'Missing or invalid CSRF token' in rv.data
+
+    def test_qunit_page(self):
+        rv = self.app.get('/qunit/')
+        assert 'QUnit Tests' in rv.data
+
+    def test_404_page(self):
+        rv = self.app.get('/i-am-not-found/')
+        assert rv.status_code == 404
 
 
 if __name__ == '__main__':
