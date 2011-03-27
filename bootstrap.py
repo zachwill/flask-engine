93c88441a81db77610effa3a199d4e3e0abe06d9
Zach Williams <hey@zachwill.com>
2011-03-26 21:29:42 -0500

    Now using werkzeug debugger for Dev server

--- bootstrap.py
+++ bootstrap.py
@@ -1,15 +1,20 @@
 """Google App Engine uses this file to run your Flask application."""
 
+import os
 from wsgiref.handlers import CGIHandler
 from utils import adjust_sys_path
 
 adjust_sys_path()
 
 from app import create_app
-
+from werkzeug_debugger_appengine import get_debugged_app
 
 def main():
     app = create_app()
+    # For more information: http://goo.gl/RNofH
+    if 'SERVER_SOFTWARE' in os.environ and os.environ['SERVER_SOFTWARE'].startswith('Dev'):
+        app.debug = True
+        app = get_debugged_app(app)
     CGIHandler().run(app)
 
 if __name__ == '__main__':
