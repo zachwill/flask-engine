6504ff65c8aa30df29f9ff6b7b2b2563abb63f79
Zach Williams <hey@zachwill.com>
2011-03-29 03:16:12 -0500

    Updated settings file. Added Flask docs link.

--- app/settings.py
+++ app/settings.py
@@ -1,3 +1,11 @@
-"""You can store your app's configuration settings here."""
+"""
+You can store your app's configuration settings here.
 
-SECRET_KEY = 'this_obviously_should_be_changed'
+Generate good secret keys:  http://flask.pocoo.org/docs/quickstart/#sessions
+    >>> import os
+    >>> os.urandom(24)
+    '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
+
+"""
+
+SECRET_KEY = 'this_obviously_should_be_changed_using_os.urandom()'
