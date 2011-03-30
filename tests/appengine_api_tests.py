4004db23bb0b5d35b4bf3aec5d8c3f047f842dd5
Zach Williams <hey@zachwill.com>
2011-03-30 18:43:49 -0500

    Now using GAE v1.4.3's testbed for testing.

---
+++ tests/appengine_api_tests.py
@@ -0,0 +1,23 @@
+"""Additional sample test file that will run along with app_tests.py"""
+
+import unittest
+from google.appengine.ext import testbed
+from google.appengine.api import urlfetch
+
+
+class AppEngineAPITest(unittest.TestCase):
+
+    def setUp(self):
+        # First, create an instance of the Testbed class.
+        self.testbed = testbed.Testbed()
+        # Then activate the testbed, which prepares the service stubs for use.
+        self.testbed.activate()
+        # Initialize urlfetch stub.
+        self.testbed.init_urlfetch_stub()
+
+    def tearDown(self):
+        self.testbed.deactivate()
+
+    def test_urlfetch(self):
+        response = urlfetch.fetch('http://www.google.com')
+        self.assertTrue(response.content.find('<html>'))
